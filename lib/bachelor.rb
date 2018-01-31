fb0bb616e97cd8072b9ed22d23fdc285b28dbf45
Stacey  Marks <staceyamarks@gmail.com>
2018-01-31 21:13:03 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,12 +1,7 @@
 def get_first_name_of_season_winner(data, season)
-  data.each do |seasons, contestant_array|
-    if seasons = season
-      seasons.each do |person, person_hash|
-        if person_hash[:status] == 'Winner'
-          
-          return person_hash[:name].split(" ")[0]
-        end
-      end
+  data[season].each do |contestant_hash|
+    if contestant_hash["status"].downcase == "winner"
+      return contestant_hash["name"].split(" ").first
     end
   end
 end
