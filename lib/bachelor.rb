dbd9d2bfcbf627404e60c2103edcb4789713dfd4
Stacey  Marks <staceyamarks@gmail.com>
2018-01-31 21:16:55 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -29,7 +29,10 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        return contestant_hash["occupation"]
 end
 
 def get_average_age_for_season(data, season)
